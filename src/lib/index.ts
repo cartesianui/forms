ba8847d6afa4b68c8e9d6135a8c94cbde6d30633
Syed Ali Murtaza <ali@kazmi.me>
2022-06-06 12:28:29 +0500

    Adds index file with lib, to manage top level exports in public-api.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,11 @@
+/*
+ * Public API Surface of ng-form
+ */
+
+export * from './ng-form.module';
+export * from './form-validator.service';
+export * from './configurable-form/configurable-form.component';
+export * from './configurable-form/models/form-section.model';
+export * from './configurable-form/models/configurable-field.model';
+export * from './configurable-form/models/form-field-select.model';
+export * from './configurable-form/models/form-field.model';
--- src/public-api.ts
+++ src/public-api.ts
@@ -2,10 +2,4 @@
  * Public API Surface of ng-form
  */
 
-export * from './lib/ng-form.module';
-export * from './lib/form-validator.service';
-export * from './lib/configurable-form/configurable-form.component';
-export * from './lib/configurable-form/models/form-section.model';
-export * from './lib/configurable-form/models/configurable-field.model';
-export * from './lib/configurable-form/models/form-field-select.model';
-export * from './lib/configurable-form/models/form-field.model';
+export * from './lib';
